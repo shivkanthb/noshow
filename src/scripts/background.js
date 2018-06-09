3f4168fe1f55b7cb753a2f22ee86f7021073e53e
shivkanthb <shivkanthb@gmail.com>
2018-06-09 13:48:41 -0700

    overlay on page load + runs only on youtube, facebook and twitter

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -11,16 +11,15 @@ ext.runtime.onMessage.addListener(
   }
 );
 
-// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-//   if (changeInfo.status == 'complete' && tab.active) {
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  
+  if (changeInfo.status == 'complete' && tab.active) {
+    
+    ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      console.log(activeTab.url);
+      chrome.tabs.sendMessage(tabs[0].id, {action: "suppress"}, function(response) {});
+    });
 
-//     console.log('the page is done. ');
-
-//     ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       var activeTab = tabs[0];
-//       console.log(activeTab.url);
-//       chrome.tabs.sendMessage(tabs[0].id, {action: "call-suppress"}, function(response) {});
-//     });
-
-//   }
-// })
+  }
+})
--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,37 +1,17 @@
 import ext from "./utils/ext";
 
 var suppress = () => {
-  // var elementsToHide = document.querySelectorAll('#notification-count');
-  // // alert(elementsToHide);
-  // console.log(elementsToHide);
-  // elementsToHide.forEach(function(ele) {
-  //   ele.setAttribute('style','display:none');
-  // });
-  console.log('Dope aff');
+  setTimeout(sNotification, 1000);
+}
 
-  var elementsToHide = document.querySelectorAll('#notification-count');
-  if (elementsToHide.length) {
-    console.log('Caught it');
-    console.log(elementsToHide);
-    elementsToHide.forEach(function(ele) {
-      ele.setAttribute('style','display:none');
-    });
-  } else {
-    console.log('Not found damnit');
-  }
-  var data = {
-    val: document.location.href
-  }
-
-  return data;
+var removeOverlay = () => {
+  var overlayElement = document.querySelector('.noshow-overlay');
+  document.documentElement.removeChild(overlayElement);
 }
 
 function onRequest(request, sender, sendResponse) {
  if (request.action === 'suppress') {
     sendResponse(suppress())
-    // console.log('Bruh');
-    // supress()
-    // sendResponse('done');
   } else if (request.action === 'call-suppress') {
     console.log('new method called');
     setTimeout(sNotification, 10);
@@ -39,25 +19,44 @@ function onRequest(request, sender, sendResponse) {
   }
 }
 
+var overlay = () => {
+  console.log('The body of the element');
+
+  var overlayElement = document.createElement('div');
+  overlayElement.setAttribute('class', 'noshow-overlay');
+  var overlayElementText = document.createElement('div');
+  overlayElementText.setAttribute('class', 'noshow-overlay-text');
+  overlayElementText.innerHTML = 'Loading....';
+  overlayElement.appendChild(overlayElementText);
+  overlayElement.setAttribute('style', 'position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; background-color: #FFF; z-index: 9999');
+  // document.body.style.backgroundColor = "green";
+  // this is so dope. being able to append to the current tab even before the body or head is available
+  document.documentElement.appendChild(overlayElement);
+  console.log(document.body);
+
+}
+
 var sNotification = () => {
+  console.log('suppressing that shit');
   var url = document.location.href;
   if(!url || !url.match(/^http/)) return;
 
   var domain = document.location.hostname;
-  var elementsToHide;
+  var elementsToHide = '';
 
   if (domain.indexOf('youtube.com') !== -1) {
+    console.log('found youtube');
     elementsToHide = document.querySelectorAll('#notification-count');
   } else if (domain.indexOf('facebook.com') !== -1) {
     elementsToHide = document.querySelectorAll('.jewelCount');
   } else if (domain.indexOf('twitter.com') !== -1) {
     elementsToHide = document.querySelectorAll('.count-inner');
   } else {
-    console.log('Ignore this link');
+    console.log('Ignoring this link');
   }
   console.log(domain);
 
-  if (elementsToHide.length) {
+  if (elementsToHide && elementsToHide.length) {
     console.log('Caught it man');
     console.log(elementsToHide);
     elementsToHide.forEach(function(ele) {
@@ -66,10 +65,13 @@ var sNotification = () => {
   } else {
     console.log('Not found in this page..' );
   }
+
+  removeOverlay();
 }
 
+overlay();
 // Call the suppress
 // sNotification();
-setTimeout(sNotification, 50);
+// setTimeout(sNotification, 1000);
 
 ext.runtime.onMessage.addListener(onRequest);
